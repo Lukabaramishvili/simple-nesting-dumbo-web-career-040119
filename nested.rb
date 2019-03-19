d7bfe764bfd6d1a09f536d322503f893d4ecf4d8
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-19 22:47:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     newly-added-to-hash ="Assembly"
-     programmer_hash[:dennis_ritchie][:languages] = newly-added-to-hash
+
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
     return programmer_hash
 end
